da16e2689f8c216533688f9537d4a68be3d6659e
Sergey Pupkin <dehter1997@rambler.ru>
2021-09-22 16:36:42 +0300

    ыы

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 $(function(){
     let content = $('.content');
-    console.log($('.content:hidden'))
     content.slice(0,4).show();
         $('#loadMore').on('click',function(e){
             e.preventDefault()
+            console.log($('.content:visible'))
             $('.content:hidden').slice(0,4).slideDown(500);
-            if($('.content:hidden').length < 1 ){
+            if($('.content:visible').length > 12){
                 $('#loadMore').off('click');
                 $('#loadMore').text('Вы хотите выше').on('click',function(){
                     $('.content:visible').slice(4,content.length).slideUp(500)
+                    $('#loadMore').text('Load more')
                 })
             } 
         })
-})
 
+        function SlideVniz(){
+            e.preventDefault()
+            $('.content:hidden').slice(0,4).slideDown(500);
+        }
+})
+/* 
+$(function(){
+    let content = $('.content')
+    $('.content:hidden').slice(0,4).show()
+    $('#loadMore').on('click',SlideVniz);
+
+    function SlideVniz(e){
+        e.preventDefault() 
+        console.log('Visible:',$('.content:visible'))
+        console.log('HIDDEN:',$('.content:hidden'))
+        if($('.content:visible').length >= 4){
+            $('.content:hidden').slice(0,4).slideDown(500);
+        }
+
+        if($('.content:visible').length >= 12){
+            $('#loadMore').text("Вы хотите выше ?").on('click',function(){
+                $('.content:visible').slice(4,content.length).slideUp(500)
+            })
+        }
+    }
+}) */
